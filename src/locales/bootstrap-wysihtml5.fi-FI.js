6d03bde079cb6ebe6b1869e722c2b4801416c56a
Niko <Niko9911@users.noreply.github.com>
2017-08-01 01:35:39 +0300

    Create bootstrap-wysihtml5.fi-FI.js

---
+++ src/locales/bootstrap-wysihtml5.fi-FI.js
@@ -0,0 +1,54 @@
+/**
+ * Finnish translation for bootstrap-wysihtml5
+ * Niko9911, Aka. Niko G. https://github.com/Niko9911
+ */
+(function($){
+  $.fn.wysihtml5.locale['fi-FI'] = {
+    font_styles: {
+      normal: 'Normaali teksti',
+      h1: 'Otsikko 1',
+      h2: 'Otsikko 2',
+      h3: 'Otsikko 3',
+      h4: 'Otsikko 4',
+      h5: 'Otsikko 5',
+      h6: 'Otsikko 6'
+    },
+    emphasis: {
+      bold: 'Lihavoitu',
+      italic: 'Kursivoitu',
+      underline: 'Alleviivattu',
+      small: 'Pieni'
+    },
+    lists: {
+      unordered: 'Järjestelemätön lista',
+      ordered: 'Järjestelty lista',
+      outdent: 'Ulonnus',
+      indent: 'Sisennus'
+    },
+    link: {
+      insert: 'Syötä linkki',
+      cancel: 'Peruuta',
+      target: 'Avaa linkki uudessa ikkunassa'
+    },
+    image: {
+      insert: 'Syötä kuva',
+      cancel: 'Peruuta'
+    },
+    html: {
+      edit: 'Muokkaa HTML'
+    },
+    colours: {
+      black: 'Musta',
+      silver: 'Hopea',
+      gray: 'Harmaa',
+      maroon: 'Ruskea',
+      red: 'Punainen',
+      purple: 'Violetti',
+      green: 'Vihreä',
+      olive: 'Oliivi',
+      navy: 'Navy',
+      blue: 'Sininen',
+      orange: 'Oranssi'
+    }
+  };
+}(jQuery));
